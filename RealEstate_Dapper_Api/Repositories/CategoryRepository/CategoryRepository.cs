e63052b7ed92e83e26498a08e5ec56286d7184dd
Mert Kaya <merteneskaya outlook.com>
2024-04-05 11:12:45 +0300

    BaglantiKonfig/Repolar

---
+++ RealEstate_Dapper_Api/Repositories/CategoryRepository/CategoryRepository.cs
@@ -0,0 +1,20 @@
+﻿using RealEstate_Dapper_Api.Dtos.CategoryDtos;
+using RealEstate_Dapper_Api.Models.DapperContext;
+
+namespace RealEstate_Dapper_Api.Repositories.CategoryRepository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly Context _context;
+
+        public CategoryRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public Task<List<ResultCategoryDto>> GetAllCategoryAsync()
+        {
+            return _context.geta
+        }
+    }
+}
