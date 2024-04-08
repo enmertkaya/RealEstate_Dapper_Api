ca930f9261008f5a8b4ec48366b6088d0ccbf699
Enes Mert Kaya <merteneskaya@outlook.com>
2024-04-08 14:46:10 +0300

    Service Api Consume

---
+++ RealEstate_Dapper_Api/Repositories/ServiceRepository/ServiceRepository.cs
@@ -0,0 +1,73 @@
+﻿using Dapper;
+using RealEstate_Dapper_Api.Dtos.ServiceDtos;
+using RealEstate_Dapper_Api.Models.DapperContext;
+
+namespace RealEstate_Dapper_Api.Repositories.ServiceRepository
+{
+    public class ServiceRepository : IServiceRepository
+    {
+        private readonly Context _context;
+        public ServiceRepository(Context context)
+        {
+            _context = context;
+        }
+        public async void CreateService(CreateServiceDto createServiceDto)
+        {
+            string query = "insert into Service (ServiceName,ServiceStatus) values (@serviceName,@serviceStatus)";
+            var parameters = new DynamicParameters();
+            parameters.Add("@serviceName", createServiceDto.ServiceName);
+            parameters.Add("@serviceStatus", true);
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
+            }
+        }
+
+        public async void DeleteService(int id)
+        {
+            string query = "Delete From Service Where ServiceID=@serviceID";
+            var parameters = new DynamicParameters();
+            parameters.Add("@serviceID", id);
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
+            }
+        }
+
+        public async Task<List<ResultServiceDto>> GetAllServiceAsync()
+        {
+            string query = "Select * From Service";
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryAsync<ResultServiceDto>(query);
+                return values.ToList();
+            }
+        }
+
+        public async Task<GetByIDServiceDto> GetService(int id)
+        {
+            string query = "Select * From Service Where ServiceID=@serviceID";
+            var parameters = new DynamicParameters();
+            parameters.Add("@serviceID", id);
+            using (var connection = _context.CreateConnection())
+            {
+                var values = await connection.QueryFirstOrDefaultAsync<GetByIDServiceDto>(query, parameters);
+                return values;
+            }
+        }
+
+        public async void UpdateService(UpdateServiceDto updateServiceDto)
+        {
+            string query = "Update Service Set ServiceName=@serviceName,ServiceStatus=@serviceStatus where ServiceID=@serviceID";
+            var parameters = new DynamicParameters();
+            parameters.Add("@serviceName", updateServiceDto.ServiceName);
+            parameters.Add("@serviceStatus", updateServiceDto.ServiceStatus);
+            parameters.Add("@serviceID", updateServiceDto.ServiceID);
+
+            using (var connectiont = _context.CreateConnection())
+            {
+                await connectiont.ExecuteAsync(query, parameters);
+            }
+        }
+    }
+}
