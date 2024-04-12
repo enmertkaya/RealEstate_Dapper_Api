a75bb9f26d184e1252216d0674847035aad5ce6a
Enes Mert Kaya <merteneskaya@outlook.com>
2024-04-12 22:32:08 +0300

    istatistik consume
    sorgularla istatistik  verileri admin sayfasina eklendi

--- RealEstate_Dapper_Api/Program.cs
+++ RealEstate_Dapper_Api/Program.cs
@@ -7,6 +7,7 @@ using RealEstate_Dapper_Api.Repositories.WhoWeAreRepository;
 using RealEstate_Dapper_Api.Repositories.PopularLocationRepositories;
 using RealEstate_Dapper_Api.Repositories.TestimonialRepositories;
 using RealEstate_Dapper_Api.Repositories.EmployeeRepositories;
+using RealEstate_Dapper_Api.Repositories.StatisticsRepositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,6 +23,7 @@ builder.Services.AddTransient<IBottomGridRepository,BottomGridRepository>();
 builder.Services.AddTransient<IPopularLocationRepository,PopularLocationRepository>();
 builder.Services.AddTransient<ITestimonialRepository,TestimonialRepository>();
 builder.Services.AddTransient<IEmployeeRepository,EmployeeRepository>();
+builder.Services.AddTransient<IStatisticsRepository,StatisticsRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
