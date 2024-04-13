5f77d059c0eaafdb89e2c5cebed6e26026145b28
Enes Mert Kaya <merteneskaya@outlook.com>
2024-04-13 17:47:28 +0300

    toDoList eklendi

--- RealEstate_Dapper_Api/Program.cs
+++ RealEstate_Dapper_Api/Program.cs
@@ -8,6 +8,8 @@ using RealEstate_Dapper_Api.Repositories.PopularLocationRepositories;
 using RealEstate_Dapper_Api.Repositories.TestimonialRepositories;
 using RealEstate_Dapper_Api.Repositories.EmployeeRepositories;
 using RealEstate_Dapper_Api.Repositories.StatisticsRepositories;
+using RealEstate_Dapper_Api.Repositories.ContactRepositories;
+using RealEstate_Dapper_Api.Repositories.ToDoListRepositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,6 +26,8 @@ builder.Services.AddTransient<IPopularLocationRepository,PopularLocationReposito
 builder.Services.AddTransient<ITestimonialRepository,TestimonialRepository>();
 builder.Services.AddTransient<IEmployeeRepository,EmployeeRepository>();
 builder.Services.AddTransient<IStatisticsRepository,StatisticsRepository>();
+builder.Services.AddTransient<IContactRepository,ContactRepository>();
+builder.Services.AddTransient<IToDoListRepository,ToDoListRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
